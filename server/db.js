5fffb07ffa47f94b0fcf8f23b2b6983888eea700
Nimrod Feldman <Nimrod.feldman@gmail.com>
2021-04-20 17:00:02 +0300

    auth is now connected to db

--- server/db.js
+++ server/db.js
@@ -5,7 +5,15 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME, 
     host: process.env.DB_HOST,
-    port: 5432
+    port: process.env.DB_PORT
 });
+
+pool.connect((err, client) => {
+    if(err){
+        throw err;
+    } else {
+        console.log(`connected to db on port ${process.env.DB_PORT}`);
+    }
+})
 
 module.exports = pool;
--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -3,9 +3,20 @@ const router = express.Router();
 const pool = require('../db');
 const bcrypt = require('bcryptjs');
 
-router.post('/login', (req, res) => {
+router.post('/signin', async (req, res) => {
     try {
-
+        let { email, password } = req.body;
+        pool.query(
+            "SELECT * FROM users WHERE email = $1 AND password = $2;",
+            [email, password],
+            (err, result) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.json("login succeded");
+                }
+            }
+        );
     } catch (error) {
         console.log(error.message);
     }
@@ -16,26 +27,22 @@ router.post('/signup', async (req, res) => {
         // SHOULD RECIEVE HASED PASSWORD
         // VALIDATION SHOULD BE MINIMAL - FRONT END VALIDATION
         let { name, email, password } = req.body;
-
         if (!name || !email || !password) {
             console.log("IMPLIMENT VALIDATION");
         }
-
-        // let salt = bcrypt.genSaltSync(10);
-        // let temp = await bcrypt.hash(password, salt);
-        // await bcrypt.hash(password, salt, (err, hash) => {
-        //     if (err) {
-        //         throw err;
-        //     }
-        //     //savepassword to DB
-        // });
-        console.log({
-            name, password, email
-        })
-
-        res.json("success");
+        pool.query(
+            "INSERT INTO users(name, email, password) VALUES ($1, $2, $3);",
+            [name, email, password],
+            (err, res) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.json("success signup");
+                }
+            }
+        );
     } catch (error) {
-        console.log(error.message);
+        throw error;
     }
 });
 
